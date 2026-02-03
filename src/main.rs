ca542b5b3a0cedb4bded2b21e9b372a1ec7ab129
hellopradeep69 <hellopradeep69@gmail.com>
2026-02-03 12:17:54 +0530

    Fix(main): use rust-analyzer
    If not possible at least mention what LSP formation or lint you are using
        so that i can switch it too :-)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 //use rand:Rng
 fn main() {
-   // let no=(1,2,3,4,5,6,7,8,9);
-   // let color=("red","green","blue","yellow");
-   // let power=("reverse","block","c_color");
+    // let no=(1,2,3,4,5,6,7,8,9);
+    // let color=("red","green","blue","yellow");
+    // let power=("reverse","block","c_color");
 
-
-    let card=(1,"red",0);
-    println!("{:?}",card)
+    let card = (1, "red", 0);
+    println!("{:?}", card)
 }
