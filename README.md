82a96ff3990ba4de7e764901bb68875cca2a5187
amark2005 <amarkeshav14@gmail.com>
2026-02-07 23:59:19 +0530

    enum struct added

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ----------
 
 - Play Uno inside Your terminal
+
+## Developer note
+--
+Bro i used enum for choice of color and struct for card as new datatype shit.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+
 //use rand:Rng
+
+#[derive(Debug)]
+enum Color {Red,Blue,Green}
+#[derive(Debug)]
+struct Card {color:Color,value:i32}
 fn main() {
     // let no=(1,2,3,4,5,6,7,8,9);
     // let color=("red","green","blue","yellow");
     // let power=("reverse","block","c_color");
-
-    let card = (1, "red", 0);
-    println!("{:?}", card)
+    let mut mycard = Card{color:Color::Red,value:7};
+    let mut deck = Vec::new();
+    deck.push(mycard);
+    //let card = (1, "red", 0);
+    println!("{:?}", deck);
 }
